f69a5dee85a7d770ee938cd2c4cbeb4fc5599377
RoeiGlamDev <Roeiglam2@gmail.com>
2025-07-04 12:38:29 +0700

    Add components/Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 Next.js App</p>
+      <p>
+        Images and videos courtesy of{' '}
+        <a href="https://www.pexels.com/" target="_blank" rel="noopener noreferrer">
+          Pexels
+        </a>
+      </p>
+    </footer>
+  );
+}
